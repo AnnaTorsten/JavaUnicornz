afc1448391920a7fc18c1e511743c9d7a87d1f5c
cteye <christoffer.teye@gmail.com>
2014-10-22 13:34:00 +0200

    Update getstations.js
    Lägger till divs runt stationer och delar in i tåg och tunnelbana och i respektive riktning.

--- getstations.js
+++ getstations.js
@@ -4,6 +4,7 @@ var getLocation = function() {			//Runs the code ASAP
 		navigator.geolocation.getCurrentPosition(function(position){
 			coordinates = position.coords.latitude + ',' + position.coords.longitude;
 			getStations(coordinates); 	//Invokes the getStation function with the users coordinates
+			console.log("Your coordinates: " + coordinates)
 		});
 	} else { 							//Returns an error mesage if the browser sucks
 		x.innerHTML = "Geolocation is not supported by this browser.";
@@ -26,8 +27,9 @@ function getStations(coordinates) {		//Uses coordinates to find nearby stations 
 			$("#result").html("");
 			var nearbystations = data.results;
 			if (nearbystations.length > 0) {
-				for (i = 0; i < 5; i++ ) {	//Itterates through nearby stations
-					getSiteId(nearbystations[i].name);
+				for (i = 0; i < 2; i++ ) {	//Itterates through nearby stations
+					getSiteId(nearbystations[i].name.toLowerCase(),i+1);
+					$("#result").append("<div id= \"station" + (i + 1) + "\"></div>"); 
 				}
 			}
 		},
@@ -38,7 +40,8 @@ function getStations(coordinates) {		//Uses coordinates to find nearby stations 
 	});
 }
 
-function getSiteId(site) {				//Finds SiteId using the nearby stations and SL Platsinfo API
+function getSiteId(site, number) {		//Finds SiteId using the nearby stations and SL Platsinfo API
+	console.log('#' + number + 'running getSiteId for ' + site);
 	$.ajax({
 		url: 'http://api.sl.se/api2/typeahead.json',
 		data: {
@@ -50,9 +53,11 @@ function getSiteId(site) {				//Finds SiteId using the nearby stations and SL Pl
 		crossDomain: 'true',
 
 		success: function(data) {
+			console.log('#' + number + 'received data in getSitdeId for ' + site);
 			var siteidstation = (data.ResponseData[0].SiteId)
+			var stationname = (data.ResponseData[0].Name)
 			if (siteidstation.length > 0) {
-				getDepartures(siteidstation)
+				getDepartures(siteidstation, stationname, number)
 			};
 		},
 
@@ -62,31 +67,36 @@ function getSiteId(site) {				//Finds SiteId using the nearby stations and SL Pl
 	});
 }
 
-function getDepartures(siteid) {		//Uses SiteId to find departures with SL Realtidsinfo API
+function getDepartures(siteid, stationname, number) {		//Uses SiteId to find departures with SL Realtidsinfo API
+	console.log('#' + number + 'calling getDepartures ' + siteid);
 	$.ajax({
 		url: 'http://api.sl.se/api2/realtimedepartures.json',
 		data: {
 			"key": '74b0060de6e2403780e6dfbacada5743',
 			"siteid": siteid,
-			"timewindow": 30			//The timewindow for departures. Too long and it seems to truncate results.
+			"timewindow": 30								//The timewindow for departures, if too long it seems to truncate results.
 		},
 		dataType: "json",
 		type: 'get',
 		crossDomain: 'true',
 
 		success: function(data) {
+			console.log('received data in getDepartures for ' + siteid);
 			var metros = data.ResponseData.Metros;
 			var trains = data.ResponseData.Trains;
-			if (metros.length > 0) {	//Checks if the station has metros
-				$("#result").append("<strong>" + metros[0].StopAreaName + "</strong><br>");
-				for (j = 0; j < metros.length; j++ ) {
-					$("#result").append(metros[j].Destination + " " + metros[j].DisplayTime + "<br>");
+			if (metros.length > 0 || trains.length > 0) {	//Checks it is a metro or train station
+				$("#station" + number).append("<h2>" + stationname + "</h2>");
+				if (metros.length > 0) {					//Checks if it is a metro station
+					$("#station" + number).append("<div class = \"metros\"><div class = \"direction1\"></div><div class = \"direction2\"></div></div>");
+					for (j = 0; j < metros.length; j++ ) { 	//Iterates through all departures
+						$("#station" + number + " > .metros > .direction" + metros[j].JourneyDirection).append("<li>" + metros[j].Destination + " " + metros[j].DisplayTime + "</li>");
+					}
 				}
-			}
-			if (trains.length > 0) {	//Checks if the station has trains
-				$("#result").append("<strong>" + trains[0].StopAreaName + "</strong><br>");
-				for (j = 0; j < trains.length; j++ ) {
-					$("#result").append(trains[j].Destination + " " + trains[j].DisplayTime + "<br>");
+				if (trains.length > 0) {					//Checks if it is a train station
+					$("#station" + number).append("<div class = \"trains\"><div class = \"direction1\"></div><div class = \"direction2\"></div></div>");
+					for (j = 0; j < trains.length; j++ ) {
+						$("#station" + number + " > .trains > .direction" + trains[j].JourneyDirection).append("<li>" + trains[j].Destination + " " + trains[j].DisplayTime + "</li>");
+					}
 				}
 			}
 		},
